2fd41fd3f63efa284c5bd2029903adf970a2cc21
thatpixelguy <mjburns93@gmail.com>
2016-03-15 18:42:01 -0500

    Added files via upload

--- projects/la3d/graphics.js
+++ projects/la3d/graphics.js
@@ -3,6 +3,21 @@
 var canvas = null;
 var context = null;
 
+var viewportWidth = 500;
+var viewportHeight = 500;
+
+var fov = 60;
+var near = 0.1;
+var far = 1000;
+var s = 1 / Math.tan(fov * 0.5 * Math.PI / 180);
+
+var perspectiveMatrix = [
+    [s, 0, 0, 0],
+    [0, s, 0, 0],
+    [0, 0, -far / (far - near), -1],
+    [0, 0, -(far * near) / (far - near), 0]
+];
+
 function setWorkingCanvas(name)
 {
     canvas = document.getElementById(name);
@@ -18,155 +33,19 @@ function drawLine(x1, y1, x2, y2, color)
     context.stroke();
 }
 
-
-class Matrix
-{
-    constructor(rows)
-    {
-        this.rows = rows;
-        this.width = this.rows[0].length;
-        this.height = this.rows.length;
-    }
-
-    add(other)
-    {
-        var rows = [];
-        for(var i = 0; i < this.height; i++)
-        {
-            rows[i] = [];
-            var row = [];
-            for(var j = 0; j < this.width; j++)
-            {
-                rows[i][j] = this.rows[i][j] + other.rows[i][j]
-            }
-        }
-        return new Matrix(rows);
-    }
-
-    multiply(other)
-    {
-        var rows = [];
-
-        if(this.width != other.height)
-        {
-            console.log("ERROR: Matrix multiplication doesn't make sense!");
-            console.log(this.width, this.height, other.width, other.height);
-            return null;
-        }
-
-        for(var i = 0; i < this.height; i++)
-        {
-            rows[i] = [];
-            for(var j = 0; j < other.width; j++)
-            {
-                rows[i][j] = 0;
-            }
-        }
-
-        for(var i = 0; i < this.height; i++)
-        {
-            for(var j = 0; j < other.width; j++)
-            {
-                for(var k = 0; k < this.width; k++)
-                {
-                    rows[i][j] += this.rows[i][k] * other.rows[k][j];
-                }
-            }
-        }
-
-        return new Matrix(rows);
-    }
-
-    scale(scalar)
-    {
-        var rows = [];
-        for(var i = 0; i < this.height; i++)
-        {
-            var row = [];
-            for(var j = 0; j < this.width; j++)
-            {
-                row.push(this.rows[i][j] * scalar);
-            }
-            rows.push(row);
-        }
-        return new Matrix(rows);
-    }
-
-    toVector()
-    {
-        var elements = [];
-        for(var i = 0; i < this.height; i++)
-        {
-            for(var j = 0; j < this.width; j++)
-            {
-                elements.push(this.rows[i][j]);
-            }
-        }
-
-        return new Vector(elements);
-    }
-}
-
-class Vector extends Matrix
-{
-    constructor(elements)
-    {
-        var rows = [];
-        for(var i = 0; i < elements.length; i++)
-        {
-            rows.push([elements[i]]);
-        }
-        super(rows);
-        this.elements = elements;
-    }
-
-    set x(value)
-    {
-        this.elements[0] = this.rows[0][0] = value;
-    }
-
-    set y(value)
-    {
-        this.elements[1] = this.rows[1][0] = value;
-    }
-
-    set z(value)
-    {
-        this.elements[2] = this.rows[2][0] = value;
-    }
-
-    get x() {
-        return this.rows[0][0];
-    }
-
-    get y() {
-        return this.rows[1][0];
-    }
-
-    get z() {
-        return this.rows[2][0];
-    }
-}
-
 class Shape
 {
     constructor(edges)
     {
-        this.edges = [];
-
-        for(var i = 0; i < edges.length; i++)
-        {
-            var edge = edges[i];
-            this.edges.push([new Vector(edge[0]), new Vector(edge[1])]);
-        }
+        this.edges = edges;
     }
 
     scale(scalar)
     {
         for(var i = 0; i < this.edges.length; i++)
         {
-            this.edges[i][0] = this.edges[i][0].scale(scalar);
-            this.edges[i][1] = this.edges[i][1].scale(scalar);
+            this.edges[i][0] = vectorScale(this.edges[i][0], scalar);
+            this.edges[i][1] = vectorScale(this.edges[i][1], scalar);
         }
     }
 
@@ -174,107 +53,69 @@ class Shape
     {
         for(var i = 0; i < this.edges.length; i++)
         {
-            this.edges[i][0].rows[0][0] += x;
-            this.edges[i][0].rows[1][0] += y;
-            this.edges[i][0].rows[2][0] += z;
+            this.edges[i][0][0] += x;
+            this.edges[i][0][1] += y;
+            this.edges[i][0][2] += z;
 
-            this.edges[i][1].rows[0][0] += x;
-            this.edges[i][1].rows[1][0] += y;
-            this.edges[i][1].rows[2][0] += z;
+            this.edges[i][1][0] += x;
+            this.edges[i][1][1] += y;
+            this.edges[i][1][2] += z;
         }
     }
 
-    draw(theta)
+    draw(x, y, z, theta)
     {
-        var transform = new Matrix([
+        var transform = [
             [1, 0, 0],
             [0, 1, 0],
             [0, 0, 1]
-        ]);
+        ];
 
-        transform = transform.multiply(new Matrix([
+        transform = matrixMultiply(transform, [
             [Math.cos(theta), 0, Math.sin(theta)],
             [0, 1, 0],
             [-Math.sin(theta), 0, Math.cos(theta)],
-        ]));
+        ]);
 
-        transform = transform.multiply(new Matrix([
+        transform = matrixMultiply(transform, [
             [1, 0, 0],
             [0, Math.cos(theta), -Math.sin(theta)],
             [0, Math.sin(theta), Math.cos(theta)]
-        ]));
-
-        var fov = 60;
-        var far = 1000;
-        var near = 0.1;
-        var s = 1 / Math.tan(fov * 0.5 * Math.PI / 180);
-
-        var viewportWidth = 500;
-        var viewportHeight = 500;
-
-        var perspectiveMatrix = new Matrix([
-            [s, 0, 0, 0],
-            [0, s, 0, 0],
-            [0, 0, -far / (far - near), -1],
-            [0, 0, -(far * near) / (far - near), 0]
         ]);
 
         for(var i = 0; i < this.edges.length; i++)
         {
-            var p1 = (transform.multiply(this.edges[i][0]));
-            var p2 = (transform.multiply(this.edges[i][1]));
+            var p1 = matrixMultiplyVector(transform, this.edges[i][0]);
+            var p2 = matrixMultiplyVector(transform, this.edges[i][1]);
 
-            var offset = new Vector([0, 0, 10]);
+            var offset = [x, y, z];
 
-            p1 = p1.add(offset).toVector();
-            p2 = p2.add(offset).toVector();
+            p1 = vectorAdd(p1, offset);
+            p2 = vectorAdd(p2, offset);
 
             var w;
 
-            var o1 = new Vector([0, 0, 0]);
-            o1.x = p1.x * perspectiveMatrix.rows[0][0] + p1.y * perspectiveMatrix.rows[1][0] + p1.z * perspectiveMatrix.rows[2][0] + perspectiveMatrix.rows[3][0];
-            o1.y = p1.x * perspectiveMatrix.rows[0][1] + p1.y * perspectiveMatrix.rows[1][1] + p1.z * perspectiveMatrix.rows[2][1] + perspectiveMatrix.rows[3][1];
-            o1.z = p1.x * perspectiveMatrix.rows[0][2] + p1.y * perspectiveMatrix.rows[1][2] + p1.z * perspectiveMatrix.rows[2][2] + perspectiveMatrix.rows[3][2];
-            w    = p1.x * perspectiveMatrix.rows[0][3] + p1.y * perspectiveMatrix.rows[1][3] + p1.z * perspectiveMatrix.rows[2][3] + perspectiveMatrix.rows[3][3];
+            var o1 = matrix44MultiplyVector3(perspectiveMatrix, p1);
+            var o2 = matrix44MultiplyVector3(perspectiveMatrix, p2);
 
-            if(w != 1)
-            {
-                o1.x /= w;
-                o1.y /= w;
-                o1.z /= w;
-            }
-
-            var o2 = new Vector([0, 0, 0]);
-            o2.x = p2.x * perspectiveMatrix.rows[0][0] + p2.y * perspectiveMatrix.rows[1][0] + p2.z * perspectiveMatrix.rows[2][0] + perspectiveMatrix.rows[3][0];
-            o2.y = p2.x * perspectiveMatrix.rows[0][1] + p2.y * perspectiveMatrix.rows[1][1] + p2.z * perspectiveMatrix.rows[2][1] + perspectiveMatrix.rows[3][1];
-            o2.z = p2.x * perspectiveMatrix.rows[0][2] + p2.y * perspectiveMatrix.rows[1][2] + p2.z * perspectiveMatrix.rows[2][2] + perspectiveMatrix.rows[3][2];
-            w    = p2.x * perspectiveMatrix.rows[0][3] + p2.y * perspectiveMatrix.rows[1][3] + p2.z * perspectiveMatrix.rows[2][3] + perspectiveMatrix.rows[3][3];
-
-            if(w != 1)
-            {
-                o2.x /= w;
-                o2.y /= w;
-                o2.z /= w;
-            }
-
-            if(o1.x < -1 || o1.x > 1 || o1.y < -1 || o1.y > 1 ||
-                o2.x < -1 || o2.x > 1 || o2.y < -1 || o2.y > 1)
+            // don't draw line segments that are outside the screen's boundaries
+            if((o1[0] < -1 || o1[0] > 1 || o1[1] < -1 || o1[1] > 1) &&
+               (o2[0] < -1 || o2[0] > 1 || o2[1] < -1 || o2[1] > 1))
                 continue;
 
-            var x1 = Math.min(viewportWidth - 1, ((o1.x + 1) * 0.5 * viewportWidth)); 
-            var y1 = Math.min(viewportHeight - 1, ((1 - (o1.y + 1) * 0.5) * viewportHeight));
+            var x1 = Math.min(viewportWidth  - 1, ((o1[0] + 1) * 0.5 * viewportWidth)); 
+            var y1 = Math.min(viewportHeight - 1, ((1 - (o1[1] + 1) * 0.5) * viewportHeight));
 
-            var x2 = Math.min(viewportWidth - 1, ((o2.x + 1) * 0.5 * viewportWidth)); 
-            var y2 = Math.min(viewportHeight - 1, ((1 - (o2.y + 1) * 0.5) * viewportHeight));
+            var x2 = Math.min(viewportWidth  - 1, ((o2[0] + 1) * 0.5 * viewportWidth)); 
+            var y2 = Math.min(viewportHeight - 1, ((1 - (o2[1] + 1) * 0.5) * viewportHeight));
 
-            var color1 = Math.floor(Math.max(p1.z - 10.3, 0) * 255);
-            var color2 = Math.floor(Math.max(p2.z - 10.3, 0) * 255);
+            var color1 = Math.floor(Math.max(p1[2] - 10.3, 0) * 255);
+            var color2 = Math.floor(Math.max(p2[2] - 10.3, 0) * 255);
 
             var gradient = context.createLinearGradient(x1, y1, x2, y2);
             gradient.addColorStop(0, "rgb(" + color1 + "," + color1 + "," + color1 + ")");
             gradient.addColorStop(1, "rgb(" + color2 + "," + color2 + "," + color2 + ")");
 
-            //console.log(percentageBack);
             drawLine(x1, y1, x2, y2, gradient);
         }
     }
@@ -285,12 +126,13 @@ var tetrahedron, cube;
 function init()
 {
     tetrahedron = new Shape([
-        [[ 0,  0,  1], [-1,  0, -1]],
-        [[-1,  0, -1], [ 1,  0, -1]],
-        [[ 1,  0, -1], [ 0,  0,  1]],
-        [[ 0,  0,  1], [ 0,  1,  0]],
-        [[-1,  0, -1], [ 0,  1,  0]],
-        [[ 1,  0, -1], [ 0,  1,  0]]
+
+        [[ 1,  0, -0.71], [-1,  0, -0.71]],
+        [[-1,  0, -0.71], [ 0,  1,  0.71]],
+        [[ 0,  1,  0.71], [ 0, -1,  0.71]],
+        [[ 0, -1,  0.71], [ 1,  0, -0.71]],
+        [[ 0, -1,  0.71], [-1,  0, -0.71]],
+        [[ 1,  0, -0.71], [ 0,  1,  0.71]]
     ]);
 
     cube = new Shape([
@@ -318,7 +160,7 @@ function init()
 
     console.log(tetrahedron);
 
-    tetrahedron.draw(2);
+    cube.draw(0, 0, 10, 2);
 
     toggleAnimation();
 }
@@ -333,7 +175,8 @@ function draw()
 
     var amt = 0;
     context.translate(amt, amt);
-    cube.draw(angle);
+    cube.draw(0, 0, 10, angle);
+    tetrahedron.draw(-4, 0, 10, angle * 3);
     context.translate(-amt, -amt);
 
 }
---
+++ projects/la3d/math.js
@@ -0,0 +1,125 @@
+
+function matrixAdd(m1, m2)
+{
+    var sum = [];
+
+    for(var i = 0; i < m1.length; i++)
+    {
+        sum[i] = [];
+        for(var j = 0; j < m1[0].length; j++)
+        {
+            sum[i][j] = m1[i][j] + m2[i][j]
+        }
+    }
+    return sum;
+}
+
+function matrixMultiply(m1, m2)
+{
+    var product = [];
+
+    if(m1[0].length != m2.length)
+    {
+        console.log("ERROR: Matrix multiplication doesn't make sense!");
+        return null;
+    }
+
+    var m1w = m1[0].length;
+    var m1h = m1.length;
+    var m2w = m2[0].length;
+    var m2h = m2.length;
+
+    for(var i = 0; i < m1h; i++)
+    {
+        product[i] = [];
+        for(var j = 0; j < m2w; j++)
+        {
+            product[i][j] = 0;
+        }
+    }
+
+    for(var i = 0; i < m1h; i++)
+    {
+        for(var j = 0; j < m2w; j++)
+        {
+            for(var k = 0; k < m1w; k++)
+            {
+                product[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+
+    return product;
+}
+
+function matrixMultiplyVector(matrix, vector)
+{
+    var product = [];
+
+    var mw = matrix[0].length;
+    var mh = matrix.length;
+
+    for(var i = 0; i < mh; i++)
+    {
+        product[i] = 0;
+        for(var j = 0; j < mw; j++)
+        {
+            product[i] += matrix[i][j] * vector[j];
+        }
+    }
+
+    return product;
+}
+
+function matrixScale(matrix, scalar)
+{
+    var scaled = [];
+
+    for(var i = 0; i < matrix.length; i++)
+    {
+        scaled[i] = [];
+        for(var j = 0; j < matrix[0].length; j++)
+        {
+            scaled[i][j] = matrix[i][j] * scalar;
+        }
+    }
+    return scaled;
+}
+
+function vectorAdd(v1, v2)
+{
+    sum = [];
+    for(var i = 0; i < v1.length; i++)
+        sum[i] = v1[i] + v2[i];
+    return sum;
+}
+
+function vectorScale(vector, scalar)
+{
+    scaled = [];
+    for(var i = 0; i < vector.length; i++)
+        scaled[i] = vector[i] * scalar;
+    return scaled;
+}
+
+function matrix44MultiplyVector3(matrix44, vector3)
+{
+    var product = [0, 0, 0];
+    product[0] = vector3[0] * matrix44[0][0] + vector3[1] * matrix44[1][0] + vector3[2] * matrix44[2][0] + matrix44[3][0];
+    product[1] = vector3[0] * matrix44[0][1] + vector3[1] * matrix44[1][1] + vector3[2] * matrix44[2][1] + matrix44[3][1];
+    product[2] = vector3[0] * matrix44[0][2] + vector3[1] * matrix44[1][2] + vector3[2] * matrix44[2][2] + matrix44[3][2];
+    w = vector3[0] * matrix44[0][3] + vector3[1] * matrix44[1][3] + vector3[2] * matrix44[2][3] + matrix44[3][3];
+
+    if(w != 1)
+    {
+        product[0] /= w;
+        product[1] /= w;
+        product[2] /= w;
+    }
+
+    return product;
+}
+
+
+
+
